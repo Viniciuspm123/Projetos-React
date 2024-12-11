b0daa47fe2dca5cb4fce26e7c9358b8f3f58db24
Vinicius Marques <vinic123pm@gmail.com>
2024-12-10 22:49:56 -0300

    Primeiro commit

---
+++ Projeto03/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+import RelogioDigital from './components/RelogioDigital'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <RelogioDigital/>
+      </div>
+    </>
+  )
+}
+
+export default App
